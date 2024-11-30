380a918da8f849953ecfeeda9d8ba9cde8a2893e
Lipatant <virgile.berrier@gmail.com>
2024-11-30 11:33:04 +0100

    feat: adding play and leave buttons to MainMenu

--- Source/Globals/SceneManager/SceneManager.gd
+++ Source/Globals/SceneManager/SceneManager.gd
@@ -6,16 +6,20 @@ func _ready() -> void:
 	_play_face_out_animation()
 
 func change_scene_to_file(path: String) -> Error:
-	_play_fade_in_animation()
+	await _play_fade_in_animation()
 	var output : Error = get_tree().change_scene_to_file(path)
 	_play_face_out_animation()
 	return output
 
 func change_scene_to_packed(packed_scene: PackedScene) -> Error:
-	_play_fade_in_animation()
+	await _play_fade_in_animation()
 	var output : Error = get_tree().change_scene_to_packed(packed_scene)
 	_play_face_out_animation()
 	return output
+
+func quit() -> void:
+	await _play_fade_in_animation()
+	get_tree().quit()
 
 func _play_fade_in_animation() -> void:
 	%AnimationPlayer.play("FadeIn")
--- Source/Scenes/MainMenu/MainMenu.gd
+++ Source/Scenes/MainMenu/MainMenu.gd
@@ -8,3 +8,6 @@ extends Node
 
 func _ready() -> void:
 	MusicManager.play(music)
+
+func _on_leave_button_pressed() -> void:
+	SceneManager.quit()
